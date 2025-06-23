e03e0e7f08c15dd2d8d037e9657b3c02effbd14b
Luccioni Vincent <vincent.luccioni@gmail.com>
2025-06-23 16:06:35 +0200

    feature/signInPage/US6, reverse Import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,8 +7,8 @@ import { RouterProvider, createBrowserRouter } from "react-router";
 
 // Import the main app component
 import App from "./App";
-import SplashScreen from "./pages/SplashScreen/SplashScreen";
 import SignInForm from "./pages/SignIn/SignInForm";
+import SplashScreen from "./pages/SplashScreen/SplashScreen";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
