e17ee13d4c0e449a96e06952738437ad5235b0da
Bamba_DIOP <khadinho@live.fr>
2025-06-23 15:10:26 +0200

    work in progress

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,5 +1,28 @@
 function Login() {
-  return <h1>Bienvenue dans la page de connexion</h1>;
+  return (
+    <main className="login-page">
+      <form>
+        <label htmlFor="email">Identifiant</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Ex: bamba@gmail.com"
+        />
+
+        <label htmlFor="password">Mot de passe</label>
+        <input type="password" name="password" placeholder="***********" />
+
+        <a href="/reset-password">mot de passe oublié ?</a>
+
+        <button type="submit">Se connecter</button>
+
+        <p>
+          Pas encore inscrit ? <a href="/register">Créer un compte</a>
+        </p>
+      </form>
+    </main>
+  );
 }
 
 export default Login;
