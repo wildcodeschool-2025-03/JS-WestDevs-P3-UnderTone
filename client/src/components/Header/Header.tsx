f5a2042ae1a55ed28a0d71148f8c78c27dedb74d
Eula-Coly <eulacoly@gmail.com>
2025-06-23 14:42:52 +0200

    desktop menu done

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,62 +1,5 @@
-import { Link } from "react-router";
 import "./Header.css";
 import { useState } from "react";
-
-/*function Header() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  return (
-    <header>
-      <nav>
-        <figure>
-          <img src="Logo.png" alt="logo" className="logo-site" />
-        </figure>
-
-        <div className="header-icone-container">
-          <Link to="/connexion">
-            <img
-              src="icone-header-connexion.png"
-              alt="icone"
-              className="icone"
-            />{" "}
-          </Link>
-
-          <button onClick={toggleMenu} type="button" id="burger-menu">
-            ☰
-          </button>
-          <ul className={`navbar-list ${isMenuOpen ? "show" : "hide"}`}>
-            <li>
-              <Link to="/" onClick={toggleMenu}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/countries" onClick={toggleMenu}>
-                Destinations
-              </Link>
-            </li>
-            <li>
-              <Link to="/saved" onClick={toggleMenu}>
-                Mes favoris
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" onClick={toggleMenu}>
-                About
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
-  );
-}
-
-export default Header;*/
 
 function Header() {
   const [showLinks, setShowLinks] = useState(false);
@@ -64,46 +7,43 @@ function Header() {
   const handleSowLinks = () => setShowLinks(!showLinks);
 
   return (
-    <>
-      <header>
-        <nav className={`navbar ${showLinks ? "show-nav" : "hide-nav"} `}>
-          <div className="navbar_logo">
-            <img
-              src="images/Logo_UnderTone.svg"
-              alt="Logo représentant un casque"
-            />
-          </div>
+    <header>
+      <nav className={`navbar ${showLinks ? "show-nav" : "hide-nav"} `}>
+        <img
+          className="logo"
+          src="images/Logo_UnderTone.svg"
+          alt="Logo représentant un casque"
+        />
 
-          <ul className="navbar_links">
-            <li className="navbar_item">
-              <Link to="/event">
-                EVENEMENT <img src="/images/+.svg" alt="" />
-              </Link>
-            </li>
+        <ul>
+          <li>
+            <a href=" ">
+              EVENEMENT <img src="/images/+.svg" alt="" />
+            </a>
+          </li>
 
-            <li>
-              <Link to="/search">RECHERCHE</Link>
-            </li>
+          <li>
+            <a href=" ">RECHERCHE</a>
+          </li>
 
-            <li>
-              <Link to="/contact">CONTACT</Link>
-            </li>
-            <li>
-              <Link to="/login">
-                <img src="images/Generic avatar.svg" alt="Connexion icon" />
-              </Link>
-            </li>
-          </ul>
-          <button
-            className="navbar_burger"
-            type="button"
-            onClick={handleSowLinks}
-          >
-            <span className="burger_bar"> </span>
-          </button>
-        </nav>
-      </header>
-    </>
+          <li>
+            <a href=" ">CONTACT</a>
+          </li>
+          <li>
+            <a href=" ">
+              <img src="images/Generic avatar.svg" alt="Connexion icon" />
+            </a>
+          </li>
+        </ul>
+        <button
+          className="navbar_burger"
+          type="button"
+          onClick={handleSowLinks}
+        >
+          <span className="burger_bar"> </span>
+        </button>
+      </nav>
+    </header>
   );
 }
 export default Header;
