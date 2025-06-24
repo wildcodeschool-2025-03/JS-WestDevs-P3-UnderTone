108b5ec7a364825704307ff7fb7fa6ee6adb8b62
Bamba_DIOP <khadinho@live.fr>
2025-06-24 15:48:02 +0200

    work in progress

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,30 +1,36 @@
+import "./Login.css";
 import { Link } from "react-router";
 
 function Login() {
   return (
     <main className="login-page">
-      <section>
+      <section className="formlogin-container">
         <h1>Connexion</h1>
-        <form>
-          <label htmlFor="email">Identifiant</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Ex: bamba@gmail.com"
-          />
-
-          <label htmlFor="password">Mot de passe</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="***********"
-          />
+        <form className="login-form">
+          <div className="label-input">
+            <label htmlFor="email">Identifiant</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              // placeholder="Ex: bamba@gmail.com"
+            />
+          </div>
+          <div className="label-input">
+            <label htmlFor="password">Mot de passe</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              // placeholder="***********"
+            />
+          </div>
 
           <Link to="/app/reset-password">mot de passe oublié ?</Link>
 
-          <button type="submit">Se connecter</button>
+          <button type="submit" className="login-button">
+            Se connecter
+          </button>
 
           <p>
             Pas encore inscrit ? <Link to="/app/register">Créer un compte</Link>
