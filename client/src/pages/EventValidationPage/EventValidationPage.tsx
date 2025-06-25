851e0e5594620d12ed0bbd55463b05302ce16011
Eula-Coly <eulacoly@gmail.com>
2025-06-25 11:01:13 +0200

    Event_validation_page create

---
+++ client/src/pages/EventValidationPage/EventValidationPage.tsx
@@ -0,0 +1,16 @@
+import "./EventValidationPage.css";
+
+function EventValidationPage() {
+  return (
+    <>
+      <img src="images/Photo event.svg" alt="un batteur avec ses instruments" />
+      <p>Vent Débarasse vous invite pour son évènement Soirée Funk !</p>
+      <p>21 juin 2025</p>
+      <p>20h</p>
+      <p>Serez-vous présent?</p>
+      <button type="button">Refuser</button>
+      <button type="button">Accepter</button>
+    </>
+  );
+}
+export default EventValidationPage;
