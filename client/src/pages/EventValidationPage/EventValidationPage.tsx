0bdffd7c4ead50400f65261930c953528660643d
Eula-Coly <eulacoly@gmail.com>
2025-06-25 11:30:06 +0200

    working progress

--- client/src/pages/EventValidationPage/EventValidationPage.tsx
+++ client/src/pages/EventValidationPage/EventValidationPage.tsx
@@ -2,15 +2,19 @@ import "./EventValidationPage.css";
 
 function EventValidationPage() {
   return (
-    <>
+    <main>
       <img src="images/Photo event.svg" alt="un batteur avec ses instruments" />
-      <p>Vent Débarasse vous invite pour son évènement Soirée Funk !</p>
-      <p>21 juin 2025</p>
-      <p>20h</p>
-      <p>Serez-vous présent?</p>
-      <button type="button">Refuser</button>
-      <button type="button">Accepter</button>
-    </>
+      <section>
+        <h1>Vent Débarasse</h1>
+        <p>vous invite pour son évènement</p>
+        <h2>Soirée Funk !</h2>
+        <h3>21 juin 2025</h3>
+        <h3>20h</h3>
+        <p>Serez-vous présent?</p>
+        <button type="button">Refuser</button>
+        <button type="button">Accepter</button>
+      </section>
+    </main>
   );
 }
 export default EventValidationPage;
