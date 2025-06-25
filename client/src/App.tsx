e768bad7212c7323fa4e6391648b811cdcc84cbf
Eula-Coly <eulacoly@gmail.com>
2025-06-25 21:33:18 +0200

    tsx done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import EventValidationPage from "./pages/EventValidationPage/EventValidationPage";
 
 function App() {
   return (
     <>
       <Header />
+      <EventValidationPage />
       <Outlet />
       <Footer />
     </>
--- client/src/pages/EventValidationPage/EventValidationPage.tsx
+++ client/src/pages/EventValidationPage/EventValidationPage.tsx
@@ -2,17 +2,27 @@ import "./EventValidationPage.css";
 
 function EventValidationPage() {
   return (
-    <main>
-      <img src="images/Photo event.svg" alt="un batteur avec ses instruments" />
+    <main className="event_validation">
+      <figure>
+        <img
+          src="images/Photo event.svg"
+          alt="un batteur avec ses instruments"
+        />
+      </figure>
+
       <section>
-        <h1>Vent Débarasse</h1>
-        <p>vous invite pour son évènement</p>
-        <h2>Soirée Funk !</h2>
+        <article>
+          <h1>Vent Débarasse</h1>
+          <p>vous invite pour son évènement</p>
+          <h2>Soirée Funk !</h2>
+        </article>
         <h3>21 juin 2025</h3>
         <h3>20h</h3>
         <p>Serez-vous présent?</p>
-        <button type="button">Refuser</button>
-        <button type="button">Accepter</button>
+        <article className="validate_event">
+          <button type="button">Refuser</button>
+          <button type="button">Accepter</button>
+        </article>
       </section>
     </main>
   );
