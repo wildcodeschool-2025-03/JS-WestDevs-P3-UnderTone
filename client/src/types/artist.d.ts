718805830805e4f07f8a04fa073500845755f201
Remy-PARMENTIER-LEMIERE <remyparmentierlemiere@hotmail.fr>
2025-06-27 21:27:10 +0200

    WIP

--- client/src/types/artist.d.ts
+++ client/src/types/artist.d.ts
@@ -1,32 +1,28 @@
 type Photo = {
   id: number;
-  imgSrc: string;
+  image: string;
   date: string;
 };
 
 type Artist = {
   user_id: number;
   name: string;
-  description: string;
-  demo: string;
-  web_site: string;
-  profile_picture: string;
-  facebook_link: string;
-  instagram_link: string;
-  x_link: string;
-  deezer_link: string;
-  spotify_link: string;
-  youtube_link: string;
-  artistPhoto?: Photo[];
-};
-
-type ArtistStyle = {
-  id: number;
-  name: string;
+  description: string | null;
+  demo: string | null;
+  web_site: string | null;
+  profile_picture: string | null;
+  facebook_link: string | null;
+  instagram_link: string | null;
+  x_link: string | null;
+  deezer_link: string | null;
+  spotify_link: string | null;
+  youtube_link: string | null;
+  artistPhotos?: Photo[];
+  styles?: StyleType[];
 };
 
 type ProfilePicturePlayFavoriteProps = {
-  profilePicture: string;
+  profilePicture: string | null;
   name: string;
-  demo: string;
+  demo?: string | null;
 };
