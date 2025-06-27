7fee735299664c698c6258409ec6862ee61872fd
Eula-Coly <eulacoly@gmail.com>
2025-06-27 11:17:25 +0200

    debug

--- client/src/pages/EventValidationPage/EventValidationPage.tsx
+++ client/src/pages/EventValidationPage/EventValidationPage.tsx
@@ -2,32 +2,35 @@ import "./EventValidationPage.css";
 
 function EventValidationPage() {
   return (
-    <main className="event_validation">
-      <figure>
+    <section className="event_validation">
+      <div>
         <img
           src="images/Photo event.svg"
           alt="un batteur avec ses instruments"
         />
-      </figure>
 
-      <section>
-        <article>
-          <h1>Vent Débarasse</h1>
-          <p>vous invite pour</p>
-        </article>
-        <article>
-          <p>son évènement</p>
-          <h2>Soirée Funk!</h2>
-        </article>
-        <h3>21 juin 2025</h3>
-        <h3>20h</h3>
-        <p>Serez-vous présent?</p>
-        <article className="validate_event">
-          <button type="button">Refuser</button>
-          <button type="button">Accepter</button>
-        </article>
-      </section>
-    </main>
+        <section className="event_text">
+          <p>
+            <strong>Vent Débarasse</strong>&nbsp;
+            <span>vous invite pour son évènement</span>&nbsp;
+            <span>
+              <strong>Soirée Funk!</strong>
+            </span>
+          </p>
+          <p>
+            <strong>21 juin 2025</strong>
+          </p>
+          <p>
+            <strong>20h</strong>
+          </p>
+          <p>Serez-vous présent?</p>
+          <article className="validate_event">
+            <button type="button">Refuser</button>
+            <button type="button">Accepter</button>
+          </article>
+        </section>
+      </div>
+    </section>
   );
 }
 export default EventValidationPage;
