452cd1607cc30c51b1342f919c9db20623f0f746
Luccioni Vincent <vincent.luccioni@gmail.com>
2025-07-08 16:53:41 +0200

    EventForm component done

--- client/src/pages/Research/Research.tsx
+++ client/src/pages/Research/Research.tsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
 import "./Research.css";
+import EventForm from "../../components/EventForm/EventForm";
 
 function Research() {
   const [selectedRole, setSelectedRole] = useState("");
-  const [radius, setRadius] = useState("1");
 
-  const handleRadius = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setRadius(e.target.value);
-  };
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedRole(e.target.value);
   };
@@ -54,50 +51,7 @@ function Research() {
         </div>
       </form>
 
-      {selectedRole === "Évènement" && (
-        <form action="Submit">
-          <div className="input-group">
-            <input
-              type="text"
-              name="town"
-              id="town"
-              required
-              autoComplete="off"
-            />
-            <label htmlFor="town">Ville</label>
-          </div>
-
-          <div className="input-group">
-            <input
-              type="range"
-              name="rayon"
-              id="rayon"
-              required
-              autoComplete="off"
-              placeholder="Rayon"
-              min={1}
-              max={50}
-              onChange={handleRadius}
-              value={radius}
-            />
-            <label htmlFor="rayon">Rayon: {radius} km</label>
-          </div>
-
-          <div className="input-group">
-            <input
-              type="date"
-              name="date"
-              id="date"
-              required
-              autoComplete="on"
-              placeholder="Date"
-            />
-            <label htmlFor="date">Date</label>
-          </div>
-
-          <button type="submit">Rechercher</button>
-        </form>
-      )}
+      {selectedRole === "Évènement" && <EventForm />}
     </main>
   );
 }
