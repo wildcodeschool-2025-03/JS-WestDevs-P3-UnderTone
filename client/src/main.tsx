1ca0296d94118f9dc87f6a7c59d947d2bc40ab76
Eula-Coly <eulacoly@gmail.com>
2025-07-09 22:42:57 +0200

    import main organize

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,12 +10,12 @@ import App from "./App";
 import Artist from "./pages/Artist/Artist";
 import ConcertPlace from "./pages/ConcertPlace/ConcertPlace";
 import Event from "./pages/Event/Event";
+import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Research from "./pages/Research/Research";
 import SignInForm from "./pages/SignIn/SignInForm";
 import SplashScreen from "./pages/SplashScreen/SplashScreen";
 import { AuthProvider } from "./services/AuthContext";
-import Home from "./pages/Home/Home";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
