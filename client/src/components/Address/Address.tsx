9bdc1dfe8c4991dcdf44c4ded5a72a273bde07b4
Eula-Coly <eulacoly@gmail.com>
2025-07-10 11:59:04 +0200

    Schedule and address displayed in concertPlace

--- client/src/components/Address/Address.tsx
+++ client/src/components/Address/Address.tsx
@@ -3,6 +3,7 @@ import "./Address.css";
 function Address(props: { address: ConcertPlace["address"] }) {
   return (
     <address className="concert-address">
+      <h3>Adresse</h3>
       {props.address.split("\n").map((line) => (
         <div key={line}>{line}</div>
       ))}
