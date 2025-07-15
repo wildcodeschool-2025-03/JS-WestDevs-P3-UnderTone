a22170224ff50c3c99edcdbc97a5937e0418eb5b
Eula-Coly <eulacoly@gmail.com>
2025-07-15 10:51:44 +0200

    working progress

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./Home.css";
 
 function Home() {
-  const [choicestatus, setChoicestatus] = useState("");
+  const [choicestatus, setChoicestatus] = useState("spectateur");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setChoicestatus(e.target.value);
@@ -85,66 +85,68 @@ function Home() {
     <main className="status-user">
       <div className="home-content">
         <img src="/images/home_image.svg" alt="Table de mixage de musique" />
+        <div className="status-user-form">
+          <form>
+            <h2>
+              <strong>Je suis un:</strong>
+            </h2>
+            <div className="radio-options">
+              <input
+                type="radio"
+                id="spectateur"
+                name="status"
+                value="spectateur"
+                onChange={handleChange}
+                checked={choicestatus === "spectateur"}
+              />
+              <label htmlFor="spectateur">
+                <span>Spectateur</span>
+              </label>
 
-        <form>
-          <h2>
-            <strong>Je suis un:</strong>
-          </h2>
-          <div className="radio-options">
-            <input
-              type="radio"
-              id="spectateur"
-              name="status"
-              value="spectateur"
-              onChange={handleChange}
-              checked={choicestatus === "spectateur"}
-            />
-            <label htmlFor="spectateur">
-              <span>Spectateur</span>
-            </label>
+              <input
+                type="radio"
+                id="artist"
+                name="status"
+                value="artist"
+                onChange={handleChange}
+                checked={choicestatus === "artist"}
+              />
+              <label htmlFor="artist">
+                <span>Artiste</span>
+              </label>
 
-            <input
-              type="radio"
-              id="artist"
-              name="status"
-              value="artist"
-              onChange={handleChange}
-              checked={choicestatus === "artist"}
-            />
-            <label htmlFor="artist">
-              <span>Artiste</span>
-            </label>
+              <input
+                type="radio"
+                id="ets-status"
+                name="status"
+                value="ets-status"
+                onChange={handleChange}
+                checked={choicestatus === "ets-status"}
+              />
+              <label htmlFor="ets-status">
+                <span>Établissement</span>
+              </label>
+            </div>
+          </form>
 
-            <input
-              type="radio"
-              id="ets-status"
-              name="status"
-              value="ets-status"
-              onChange={handleChange}
-              checked={choicestatus === "ets-status"}
-            />
-            <label htmlFor="ets-status">
-              <span>Établissement</span>
-            </label>
-          </div>
-        </form>
-        {choicestatus && <div className="message-zone">{getMessage()}</div>}
+          {choicestatus && <div className="message-zone">{getMessage()}</div>}
 
-        <section className="home-buttons">
-          <div>
-            <button type="button">
-              <strong>Inscription</strong>
-            </button>
-            <button type="button">
-              <strong>Connexion</strong>
-            </button>
-          </div>
-          <div>
-            <button type="button">
-              <strong>Accéder à l'application</strong>
-            </button>
-          </div>
-        </section>
+          <section className="home-buttons">
+            <div>
+              <button type="button">
+                <strong>Inscription</strong>
+              </button>
+              <button type="button">
+                <strong>Connexion</strong>
+              </button>
+            </div>
+            <div>
+              <button type="button">
+                <strong>Accéder à l'application</strong>
+              </button>
+            </div>
+          </section>
+        </div>
       </div>
     </main>
   );
