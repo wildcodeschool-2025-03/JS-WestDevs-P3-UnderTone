4c2b22713da35104b36dc7b296a8cb2cdbee2fca
Luccioni Vincent <vincent.luccioni@gmail.com>
2025-07-18 09:36:16 +0200

    fix css

--- client/src/components/ArtistSearchResult/ArtistSearchResult.tsx
+++ client/src/components/ArtistSearchResult/ArtistSearchResult.tsx
@@ -4,27 +4,25 @@ function ArtistSearchResult({
   artistList,
 }: { artistList: FilteredArtistList[] }) {
   return (
-    <section className="result">
-      <ul>
-        {artistList.length ? (
-          artistList.map((artist) => (
-            <li key={artist.id}>
-              <img src={artist.profile_picture} alt={artist.name} />
-              <div className="artist-info">
-                <h2>{artist.name}</h2>
-                <p>
-                  {artist.musicStyles && artist.musicStyles.length > 0
-                    ? artist.musicStyles.map((style) => style.name).join(" - ")
-                    : []}
-                </p>
-              </div>
-            </li>
-          ))
-        ) : (
-          <li>Aucun artiste ne correspond à la recherche</li>
-        )}
-      </ul>
-    </section>
+    <ul className="result">
+      {artistList.length ? (
+        artistList.map((artist) => (
+          <li key={artist.id}>
+            <img src={artist.profile_picture} alt={artist.name} />
+            <div className="artist-info">
+              <h3>{artist.name}</h3>
+              <p>
+                {artist.musicStyles && artist.musicStyles.length > 0
+                  ? artist.musicStyles.map((style) => style.name).join(" - ")
+                  : []}
+              </p>
+            </div>
+          </li>
+        ))
+      ) : (
+        <li>Aucun artiste ne correspond à la recherche</li>
+      )}
+    </ul>
   );
 }
 
