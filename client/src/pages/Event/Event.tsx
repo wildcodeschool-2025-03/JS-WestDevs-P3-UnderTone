a2418c340123545367d0e001c00ee97b37cf8bcf
Luccioni Vincent <vincent.luccioni@gmail.com>
2025-07-28 17:21:50 +0200

    work in progress

--- client/src/pages/Event/Event.tsx
+++ client/src/pages/Event/Event.tsx
@@ -2,6 +2,7 @@ import { Link, useParams } from "react-router";
 import "./Event.css";
 import { useEffect, useState } from "react";
 import FavoriteButton from "../../components/FavoriteButton/FavoriteButton";
+import { useAuth } from "../../services/AuthContext";
 function Event() {
   const { id } = useParams();
   const [event, setEvent] = useState<null | EventData>(null);
@@ -14,45 +15,51 @@ function Event() {
         setEvent(data);
       });
   }, [id]);
-
+  const { isLogged } = useAuth();
   return (
     <main className="event-container">
       {event && (
         <>
-          <h1>{event.name}</h1>
-          <p>
-            {event.date.toLocaleDateString()} à {event.hour}
-          </p>
-          <p>{event.description}</p>
-
-          <div className="event-image">
-            <FavoriteButton />
-            <img src={event.image} alt="affiche de la soirée" />
-          </div>
-          <h2>Artistes invités</h2>
-          <ul className="event-artists">
-            {event.invitedArtists.map((artist) => (
-              <li key={artist.id}>
-                <Link to={`/app/artist/${artist.id}`}>
-                  <img
-                    src={artist.profilePicture}
-                    alt={`portrait de ${artist.name}`}
-                  />
-                  <div className="artist-info">
-                    <h3>{artist.name}</h3>
-                    <p>
-                      {artist.musicStyles.map((musicStyle) => (
-                        <li key={musicStyle.id}>{musicStyle.name}</li>
-                      ))}
-                    </p>
-                  </div>
-                </Link>
-              </li>
-            ))}
-          </ul>
-
-          <p>{event.concertPlaceName}</p>
-          <p>{event.address}</p>
+          <section>
+            <h1>{event.name}</h1>
+            <p>
+              {event.date.toLocaleDateString()} à {event.hour}
+            </p>
+            <h2>Artistes invités</h2>
+            <ul className="event-artists">
+              {event.invitedArtists.map((artist) => (
+                <li key={artist.id}>
+                  <Link to={`/app/artist/${artist.id}`}>
+                    <img
+                      src={artist.profilePicture}
+                      alt={`portrait de ${artist.name}`}
+                    />
+                    <div className="artist-info">
+                      <h3>{artist.name}</h3>
+                      <p>
+                        {artist.musicStyles.map((musicStyle) => (
+                          <li key={musicStyle.id}>{musicStyle.name}</li>
+                        ))}
+                      </p>
+                    </div>
+                  </Link>
+                </li>
+              ))}
+            </ul>{" "}
+            <iframe src={event.menu} title="menu" width={700} height={500} />
+            <p>{event.menu}</p>
+          </section>
+          <section>
+            <div className="event-image">
+              {isLogged && <FavoriteButton />}
+              <img src={event.image} alt="affiche de la soirée" />
+            </div>
+            <div className="event-place">
+              <p>{event.concertPlaceName}</p>
+              <p>{event.address}</p>
+              <p>{event.description}</p>
+            </div>
+          </section>
         </>
       )}
     </main>
